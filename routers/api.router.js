e64b12b52fda4d84dafaefa8b3ea76e68ebd1583
lancaster-coder1991 <grjscott1991@hotmail.com>
2020-10-23 08:55:18 +0100

    redeploy to Heroku

--- routers/api.router.js
+++ routers/api.router.js
@@ -3,10 +3,12 @@ const topicsRouter = require("./topics.router");
 const usersRouter = require("./users.router");
 const articlesRouter = require("./articles.router");
 const commentsRouter = require("./comments.router");
+const { handle404s } = require("../errors");
 
 apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/users", usersRouter);
 apiRouter.use("/articles", articlesRouter);
 apiRouter.use("/comments", commentsRouter);
+apiRouter.use("/*", handle404s);
 
 module.exports = apiRouter;
